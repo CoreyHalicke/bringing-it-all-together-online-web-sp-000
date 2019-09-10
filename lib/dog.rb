7b50f58e2b19aa52e8bbdf60471514eeb4808c68
Corey H <c.l.halicke@gmail.com>
2019-09-10 21:49:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,6 +63,7 @@ class Dog
     sql = "SELECT * FROM dogs WHERE name = ? AND breed = ?"
     result = DB[:conn].execute(sql, name, breed)[0]
     if result[0]
+      result
     else
       binding.pry
 
